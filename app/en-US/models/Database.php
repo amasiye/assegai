739f71b7578b8ffc21568a56120f4fdbb1669a7a
Andrew Masiye <amasiye313@gmail.com>
2016-08-13 21:23:33 +0200

    Updated Database model. Fleshed out properties and added method shells for select, insert, update and delete.

--- app/en-US/models/Database.php
+++ app/en-US/models/Database.php
@@ -7,16 +7,64 @@
 class Database
 {
   private $conn;
+  private $mysqli;
+  private $host;
+  private $user;
+  private $password;
+  private $name;
 
-  function __construct()
+  public $instance;
+
+  /**
+   * Constructs a Database object.
+   */
+  function __construct($db_host, $db_user, $db_pass, $db_name)
   {
+    $this->host = $db_host;
+    $this->user = $db_user;
+    $this->password = $db_pass;
+    $this->name = $db_name;
 
+    $this->mysqli =
+      new mysqli($this->host, $this->user, $this->password, $this->name);
+
+    $this->conn = $this->mysqli;
+
+    # Create singleton
+    $instance = $this;
   } // end __construct()
 
-  public function select($fields="*", $table=null, $)
+  /**
+   * Selects data from the database.
+   * @param $table String name of the table to make the selection from.
+   * @param $columns String A comma seperated list of column names to select.
+   * @param $options array An array of options for the selection.
+   */
+  public function select($table, $columns, $options=array())
   {
 
   } // end select(string, string, string)
+
+  /**
+   * Inserts data into the database.
+   */
+  public function insert($table, $columns, $values, $filter)
+  {
+
+  } // end insert()
+
+  /**
+   *
+   */
+  public function update()
+  {
+
+  } // end update()
+
+  public function delete()
+  {
+
+  }
 } // end Database
 
 ?>
