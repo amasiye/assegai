9df18c2b79251725fb5602d15eb7d24e5f68269c
Andrew Masiye <amasiye313@gmail.com>
2016-11-08 02:21:41 -0400

    Add Client Side Controllers
    Add analytics, display-options, layout-editor, mail, media, page-editor,
    setttings controller files.

---
+++ js/controllers/display-options.js
@@ -0,0 +1,63 @@
+var requestState, requestStatus;
+var Status = {
+                200:"OK",
+                2001:"Pass",
+                4000:"Cancel",
+                4001:"Fail"
+              };
+
+$('document').ready(function() {
+    // Screen Options
+    $('[name=select-showing]').change(function() {
+      console.log('Now showing: ' + $(this).val());
+      var q = location.search;
+      if(q.length > 0)
+      {
+        if(q.search('&show=') < 0)
+        {
+          q += '&show=' + $(this).val();
+        }
+        else
+        {
+          q = q.replace(/&show=\d+/, '&show=' + $(this).val());
+        }
+      }
+      else
+      {
+        q += '?show=' + $(this).val();
+      }
+      location.search = q;
+    });
+
+    $('[name=display-grid]').click(function(event) {
+      event.preventDefault();
+      var q = location.search;
+      if(q.length > 0)
+      {
+        q = (q.search(/display=\w+/) < 0)? q + '&display=grid' : q = q.replace(/display=\w+/, 'display=grid');
+      }
+      else
+      {
+        q += '?display=grid';
+      }
+      location.search = q;
+
+      return false;
+    });
+
+    $('[name=display-list]').click(function(event) {
+      event.preventDefault();
+      var q = location.search;
+      if(q.length > 0)
+      {
+        q = (q.search(/display=\w+/) < 0)? q + '&display=list' : q = q.replace(/display=\w+/, 'display=list');
+      }
+      else
+      {
+        q += '?display=list';
+      }
+      location.search = q;
+
+      return false;
+    });
+});
---
+++ js/controllers/media.js
@@ -0,0 +1,11 @@
+var requestState, requestStatus;
+var Status = {
+                200:"OK",
+                2001:"Pass",
+                4000:"Cancel",
+                4001:"Fail"
+              };
+
+$('document').ready(function() {
+  
+});
