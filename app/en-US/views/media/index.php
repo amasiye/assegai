9de0b2a96e386566d26bdbd237ad4c04cdc274e1
Andrew Masiye <amasiye313@gmail.com>
2016-11-29 17:11:47 -0400

    Update Media view
    Replaced toolbar markup with 'includes/toolbar-list.php'
    Updated list view markup.

--- app/en-US/views/media/index.php
+++ app/en-US/views/media/index.php
@@ -22,50 +22,72 @@ require_once "includes/admin-header.php";
           </h2>
         </div>
 
+        <!-- Feedback Alert -->
+        <div id="feedback-media" class="alert hidden"></div>
+
         <!-- Toolbar -->
+        <?php include_once VIEWSPATH . "includes/toolbar-list.php"; ?>
 
-        <!-- Media Items -->
-        <table class="table table-bordered">
-          <tr>
-            <th class="col-sm-1">
-              <input type="checkbox" name="media-select-all" id="media-select-all-top">
-            </th>
-            <th class="col-sm-7">File</th>
-            <th class="col-sm-2">Author</th>
-            <th class="col-sm-2">Modified</th>
-          </tr>
+        <?php if (!empty($media)): ?>
+          <!-- Media Items -->
+          <?php if (isset($_GET['display']) && streq($_GET['display'], 'grid')): ?>
 
-          <?php for($x = 0; $x < count($media); $x++) { ?>
-          <tr>
-            <td>
-              <input type="checkbox" name="media-select-{$x}" id="media-select-{$x}">
-            </td>
-            <td>
-              <div class="media">
-                <div class="media-left">
-                  <a href="admin/media/edit/<?= $media[$x]->id; ?>">
-                    <img src="<?= $media[$x]->thumb; ?>" class="media-object" alt="<?= $media[$x]->title; ?>" width="64">
-                  </a>
-                </div>
-                <div class="media-body">
-                  <h4 class="media-heading"><?= $media[$x]->title; ?></h4>
-                  <p><?= $media[$x]->filename; ?></p>
-                </div>
-              </div>
-            </td>
-            <td><?= $media[$x]->author_name; ?></td>
-            <td><?= TimeManager::get_time_since(new DateTime($media[$x]->modified)); ?></td>
-          </tr>
-          <?php } ?>
-          <tr>
-            <th>
-              <input type="checkbox" name="media-select-all" id="media-select-all-bottom">
-            </th>
-            <th>File</th>
-            <th>Author</th>
-            <th>Modified</th>
-          </tr>
-        </table>
+          <?php else: ?>
+            <table class="table table-bordered">
+              <tr>
+                <th class="col-sm-1">
+                  <input type="checkbox" name="media-select-all" id="media-select-all-top">
+                </th>
+                <th class="col-sm-7">File</th>
+                <th class="col-sm-2">Author</th>
+                <th class="col-sm-2">Modified</th>
+              </tr>
+
+              <?php for($x = 0; $x < count($media); $x++) { ?>
+                <tr>
+                  <td>
+                    <input type="checkbox" name="media-select-{$x}" id="media-select-{$x}">
+                  </td>
+                  <td>
+                    <div class="media">
+                      <div class="media-left">
+                        <a href="admin/media/edit/<?= $media[$x]->id; ?>">
+                          <img src="<?= $media[$x]->thumb; ?>" class="media-object" alt="<?= $media[$x]->title; ?>" width="64">
+                        </a>
+                      </div>
+                      <div class="media-body">
+                        <h4 class="media-heading"><?= $media[$x]->title; ?></h4>
+                        <p><?= $media[$x]->mime_type; ?></p>
+                        <div class="btn-group pull-right">
+                          <button type="button" name="btn-edit-<?= $media[$x]->id; ?>"
+                            id="btn-edit-<?= $media[$x]->id; ?>" class="btn btn-xs btn-inverse" Title="Edit">
+                            <span class="glyphicon glyphicon-pencil"></span>
+                          </button>
+                          <button type="button" name="btn-trash-<?= $media[$x]->id; ?>"
+                            id="btn-trash-<?= $media[$x]->id; ?>" class="btn btn-xs btn-default" title="Send to Trash Bin">
+                            <span class="glyphicon glyphicon-trash"></span>&nbsp;
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td><?= $media[$x]->author_name; ?></td>
+                  <td><?= TimeManager::get_time_since(new DateTime($media[$x]->modified)); ?></td>
+                </tr>
+                <?php } ?>
+                <tr>
+                  <th>
+                    <input type="checkbox" name="media-select-all" id="media-select-all-bottom">
+                  </th>
+                  <th>File</th>
+                  <th>Author</th>
+                  <th>Modified</th>
+                </tr>
+              </table>
+            <?php endif; ?>
+          <?php else: ?>
+          <p>No media items found.</p>
+        <?php endif; ?>
 
       </div><!--#end col-sm-10 (right panel) -->
 
