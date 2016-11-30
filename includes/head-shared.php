0243bdd87622bdbdcabfc63661a3d8292caafd21
Andrew Masiye <amasiye313@gmail.com>
2016-11-30 03:29:47 -0400

    Updated head-shared.php
    Disabled local stylesheet.

--- includes/head-shared.php
+++ includes/head-shared.php
@@ -39,7 +39,7 @@ $client_side_controllers = $data['client_side_controllers']; ?>
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
   <!-- jQuery UI -->
