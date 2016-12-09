2694db937d78f30b4bca38fd5da392db56b2768a
Andrew Masiye <amasiye313@gmail.com>
2016-12-09 10:58:09 -0400

    Update StatusCodes
    Add new Status Code definitions and messages for media/file upload
    proceedures.

--- app/en-US/core/StatusCodes.php
+++ app/en-US/core/StatusCodes.php
@@ -17,6 +17,12 @@ define('QUERY_STMT_ERR', 311);
 define('QUERY_STMT_PARAM_ERR', 312);
 define('FILE_WRITE_OK', 400);
 define('FILE_WRITE_ERR', 401);
+define('FILE_UPLOAD_OK', 410);
+define('FILE_UPLOAD_ERR', 411);
+define('FILE_UPLOAD_ERR_MAX_SIZE', 412);
+define('FILE_UPLOAD_ERR_MIME_TYPE', 413);
+define('FILE_UPLOAD_ERR_EXISTS', 414);
+define('FILE_UPLOAD_VALID', 419);
 define('API_REQUEST_OK', 500);
 define('API_REQUEST_ERR', 501);
 define('API_REQUEST_ERR_MISSING_KEY', 502);
@@ -35,4 +41,18 @@ define('AUTH_OK', 900);
 define('AUTH_ERR', 901);
 define('AUTH_ERR_CRED', 902);
 define('AUTH_DENIED', 910);
+define('POST_OK', 1000);
+define('POST_ERR', 1001);
+
+/* Status Code Messages */
+define('MSG_QUERY_EXEC_OK',   'Query was successfuly executed.');
+define('MSG_QUERY_EXEC_ERR',  'Query execution failed.');
+define('MSG_FILE_UPLOAD_OK',  'File upload was successful.');
+define('MSG_FILE_UPLOAD_ERR', 'File upload error.');
+define('MSG_FILE_UPLOAD_ERR_MAX_SIZE', 'Exceeded maximum allowable file upload size.');
+define('MSG_FILE_UPLOAD_ERR_MIME_TYPE', 'Invalid file type.');
+define('MSG_FILE_UPLOAD_ERR_EXISTS', 'The file already exists.');
+define('MSG_FILE_UPLOAD_VALID', 'File is valid.');
+define('MSG_API_REQUEST_OK', 'Successful API request.');
+define('MSG_API_REQUEST_ERR', 'Bad API request.');
 ?>
